f4588899da7dae8831de61c9f7d858134b044da0
Sofia Pacheco <spacheco1@uc.cl>
2020-05-16 23:47:00 -0400

    Created gitignore

---
+++ build/precache-manifest.e2e2501184f080b58ecfb8c5d7cb314d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83eb1c36cd02232ba663fda2c228875a",
+    "url": "/stockExchange/index.html"
+  },
+  {
+    "revision": "e9e69d69a2d34b081fbf",
+    "url": "/stockExchange/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ce834e6e867dcd9ef8bd",
+    "url": "/stockExchange/static/css/main.c1c68314.chunk.css"
+  },
+  {
+    "revision": "e9e69d69a2d34b081fbf",
+    "url": "/stockExchange/static/js/2.c7eb37b7.chunk.js"
+  },
+  {
+    "revision": "c04130d8b6692f26b622ca7bd7d5f6b5",
+    "url": "/stockExchange/static/js/2.c7eb37b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce834e6e867dcd9ef8bd",
+    "url": "/stockExchange/static/js/main.34ff03be.chunk.js"
+  },
+  {
+    "revision": "9246b36864b86378a5b2",
+    "url": "/stockExchange/static/js/runtime-main.c958a489.js"
+  }
+]);
